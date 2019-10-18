650f5355c63330b01186344fbd6a0cf537e0144d
mwas <francismwangi152@gmail.com>
2019-10-18 16:36:11 +0300

    refactoring to save actual message not class name

--- src/main/kotlin/com/example/demo/Listeners.kt
+++ src/main/kotlin/com/example/demo/Listeners.kt
@@ -15,22 +15,23 @@ class Listeners {
     @Autowired
     private lateinit var eventMessageRepository: EventMessageRepository
 
-    fun saveSqsMessage(sqsMessage: String){
+    fun saveSqsMessage(sqsMessage: SQSTextMessage){
+
         eventMessageRepository.save(EventMessageEntity().apply {
             id = UUID.randomUUID().toString()
-            message = sqsMessage
+            message = sqsMessage.text
         })
     }
 
     @JmsListener(destination = "\${sqs.foo}")
     fun fooListener(sqsTextMessage: SQSTextMessage) {
         logger.info("sqs foo received and event:", sqsTextMessage)
-        saveSqsMessage(sqsTextMessage.toString())
+        saveSqsMessage(sqsTextMessage)
     }
 
     @JmsListener(destination = "\${sqs.bar}")
     fun barListener(sqsTextMessage: SQSTextMessage) {
         logger.info("sqs bar received and event:", sqsTextMessage)
-        saveSqsMessage(sqsTextMessage.toString())
+        saveSqsMessage(sqsTextMessage)
     }
 }
