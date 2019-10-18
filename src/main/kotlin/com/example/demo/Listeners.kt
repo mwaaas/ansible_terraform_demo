937c77fd10766843835191d79d56bbc9f3881a0d
mwas <francismwangi152@gmail.com>
2019-10-18 09:59:14 +0300

    added jms listeners for testing

--- src/main/kotlin/com/example/demo/Listeners.kt
+++ src/main/kotlin/com/example/demo/Listeners.kt
@@ -3,17 +3,20 @@ package com.example.demo
 import com.amazon.sqs.javamessaging.message.SQSTextMessage
 import org.springframework.jms.annotation.JmsListener
 import org.springframework.stereotype.Component
+import org.slf4j.LoggerFactory
 
 @Component
 class Listeners {
 
+    private val logger = LoggerFactory.getLogger(this.javaClass)
+
     @JmsListener(destination = "\${sqs.foo}")
     fun fooListener(jsonEvent: SQSTextMessage) {
-
+        logger.info("sqs foo received and event:", jsonEvent)
     }
 
     @JmsListener(destination = "\${sqs.bar}")
     fun barListener(jsonEvent: SQSTextMessage) {
-
+        logger.info("sqs bar received and event:", jsonEvent)
     }
 }
