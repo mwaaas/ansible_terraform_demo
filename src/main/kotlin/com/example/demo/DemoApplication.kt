0e55273359a0f4db766affda6d8acf9deb271e54
mwas <francismwangi152@gmail.com>
2019-10-18 09:58:53 +0300

    removed unused import

--- src/main/kotlin/com/example/demo/DemoApplication.kt
+++ src/main/kotlin/com/example/demo/DemoApplication.kt
@@ -2,8 +2,6 @@ package com.example.demo
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.jms.annotation.JmsListener
-
 
 @SpringBootApplication
 class DemoApplication()
